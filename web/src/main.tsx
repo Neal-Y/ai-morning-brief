080d2325b6dc48538f14a0f9f835c37b261935aa
neal <neal0420yang@gmail.com>
2026-04-24 15:49:34 +0800

    fix: web tsconfig vite/client types and Uint8Array cast for push subscribe

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = atob(base64)
-  return new Uint8Array([...rawData].map((c) => c.charCodeAt(0)))
+  return new Uint8Array([...rawData].map((c) => c.charCodeAt(0))) as Uint8Array<ArrayBuffer>
 }
 
 async function setupPushNotifications(): Promise<void> {
