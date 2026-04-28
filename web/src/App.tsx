8cf55d4176a722e8a91eb7c832eeb66cb83e301b
neal <neal0420yang@gmail.com>
2026-04-28 13:15:25 +0800

    test app bottom

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -482,7 +482,7 @@ export default function App() {
             style={{
               position: 'fixed', left: 0, right: 0, bottom: 0, zIndex: 20,
               background: T.bg,
-              paddingBottom: 8,
+              paddingBottom: 0,
               display: 'flex', justifyContent: 'center',
             }}
           >
